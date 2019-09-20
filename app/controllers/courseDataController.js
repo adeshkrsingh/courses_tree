5255e9e64e3a2ff8fcaf44aa7e8a761f17297445
Adesh Singh <adesh_singh@outlook.com>
2019-09-20 23:56:34 +0530

    function structure inprogress

--- app/controllers/courseDataController.js
+++ app/controllers/courseDataController.js
@@ -1,8 +1,42 @@
 var CourseDataModel = require('../models/courseDataModel');
 
 class CourseDataController {
-    getSample() {
+    getCoursesList() {
         CourseDataModel.getCoursesList();
+    }
+    insertNewCourseList() {
+        CourseDataModel.getCoursesList();
+    }
+    editCourseListItem() {
+        CourseDataModel.getCoursesList();
+    }
+    updateCourseListItem() {
+        console.log('update courses list item')
+    }
+    softDeleteCourseItem() {
+        console.log('soft delete course item executed')
+    }
+    activateCourseListItem() {
+        console.log('activate course list item')
+    }
+    purchaseCourseItem() {
+        console.log('purchase course item executed')
+    }
+    
+    getCoursesList() {
+        CourseDataModel.getCoursesList();
+    }
+    getCoursesList() {
+        CourseDataModel.getCoursesList();
+    }
+    getLessonsList() {
+        console.log('get Lessons list')
+    }
+    getTopicsList() {
+        console.log('get Topics list')
+    }
+    getArticleList() {
+        console.log('get Article List ')
     }
 }
 
